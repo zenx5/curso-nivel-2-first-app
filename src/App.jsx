084a30ad98434fc8d638cd54bf8e262c45bf1805
Octavio Martinez <omartinez1618@gmail.com>
2024-03-28 20:46:18 -0400

    ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 
-import Parrefe from './components/Parrafe'
-import Card from './components/Card'
-
-
-
-
 export default function App() {
 
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold">Hola mundo</h1>
-      <p className="italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad quos voluptatum id ab odit reiciendis cupiditate nemo eaque animi provident sint, rerum non sed aperiam rem consequuntur vel molestiae!</p>
-
-    </>
-  )
+  return <div className="flex flex-col justify-center items-center w-screen h-screen">
+    <h1 className="text-3xl font-bold">Hola mundo</h1>
+    <p className="italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad quos voluptatum id ab odit reiciendis cupiditate nemo eaque animi provident sint, rerum non sed aperiam rem consequuntur vel molestiae!</p>
+  </div>
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,4 @@
-import './styles.css'
+
 
 export default function Card(props) {
     const { title, children } = props
