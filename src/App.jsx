998dfc80ec02a39c258cb08c4c250152cbb05e12
Octavio Martinez <omartinez1618@gmail.com>
2024-03-28 21:58:45 -0400

    ThemeState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import ThemeState from "./views/ThemeState";
 
 export default function App() {
 
+  return <div style={{ alignContent:'center'}} className="w-screen h-screen px-80 flex flex-col item justify-center gap-2">
+    <ThemeState />
+    {/* A partir de aqui nuestro codigo */}
 
-  return <div className="flex flex-col justify-center items-center w-screen h-screen">
-    <h1 className="text-3xl font-bold">Hola mundo</h1>
-    <p className="italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad quos voluptatum id ab odit reiciendis cupiditate nemo eaque animi provident sint, rerum non sed aperiam rem consequuntur vel molestiae!</p>
   </div>
+
 }
---
+++ src/views/ThemeState.jsx
@@ -0,0 +1,10 @@
+export default function ThemeState(){
+
+    return <>
+        <h1 className="text-3xl font-bold">Hooks: <span className="text-red-500">useState</span></h1>
+        <p className="italic">useState es un Hook de React que te permite agregar una <span className="font-bold">variable de estado</span> a tu componente.</p>
+        <code className="text-center w-full mt-4">
+        <span className="text-red-400">const</span> [state, setState] = <span className="text-purple-400">useState</span>(initialState)
+        </code>
+    </>
+}
